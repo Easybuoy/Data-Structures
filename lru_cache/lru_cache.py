8a31139eaecd7ce89fd881b7dec65dbd5879a13d
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-15 15:51:25 +0100

    solve lru_cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,6 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +9,12 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.currentNumberOfNode = 0
+        self.list = DoublyLinkedList()
+        self.storage = {}
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +23,13 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+        if key in self.storage.keys():
+            self.list.move_to_front(self.list.)
+            return self.storage[key]
+
+        return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +41,29 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+        if key in self.storage.keys():
+            self.storage[key] = value
+        # if (self.storage[key]):
+        #     print(self.storage[key])
+        #     self.storage[key] = value
+        #     self.list.add_to_head((key, value))
+        else:
+
+            # print(self.stora[key])
+            if (self.currentNumberOfNode < self.limit):
+                self.list.add_to_head((key, value))
+                self.storage[key] = value
+                self.currentNumberOfNode += 1
+            else:
+                self.list.remove_from_tail()
+                self.list.add_to_head((key, value))
+                self.storage[key] = value
+                self.currentNumberOfNode += 1
+
+                # remove oldest item, then add latest
+            # if (self.)
+            # if limit is reached, remove the oldest entry before adding new one
+            # increment current number of nodes
+
